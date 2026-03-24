d29dd5d9fafbe6b11727f456ddbb037cab3f20b5
Piyush Kumar <iamkpiyush@gmail.com>
2026-03-24 09:45:00 +0530

    addding more

--- run_data_generator.py
+++ run_data_generator.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     argparser.add_argument("--num_samples", type=int, required=True, help="Number of training samples to generate")
     argparser.add_argument("--split", type=str, default="train", choices=["train", "test"], help="Whether to generate training or test data")
     argparser.add_argument("--type", type=str, default="offline", choices=["offline", "online"], help="Whether to generate data for offline learning or online learning ")
-
     argparser.add_argument("--discounted_step", type=int, default=0, help="Number of initial steps to discount in the path for training data generation")
 
     args = argparser.parse_args()
@@ -21,7 +20,11 @@ if __name__ == "__main__":
     density = 5
     timeout = 300
 
-    file_path = f"data/wrp_data_16x16_{num_samples}_samples_SP_{split}.pt"
+    if data_type == "offline":  
+        file_path = f"data/wrp_data_16x16_{num_samples}_samples_SP_{split}.pt"
+    else:
+        file_path = f"data/wrp_online_data_16x16_{num_samples}_samples_SP_{split}.pt"
+
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     if os.path.exists(file_path):
         print(f"File {file_path} already exists. Please delete it or choose a different configuration.")
